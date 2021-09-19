0d8ffb2f293b05412255c3f2e25dbd12f1d2b389
sieunlee <dltldmsm@gmail.com>
2021-09-19 18:13:06 +0900

    210919

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,5 +1,9 @@
-const Hello = ()=> {
-    <p>Hello</p>
-};
+// const Hello = ()=> {
+//     <p>Hello</p>
+// };
 
-export default Hello;
+// export default Hello;
+
+export default function Hello() {
+    <p>Hello</p>
+}
