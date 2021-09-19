5e50f82903b742b0b1138c5d0f83407653a870ed
sieunlee <dltldmsm@gmail.com>
2021-09-19 18:24:32 +0900

    210919

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Hello from './component/Hello';
+import Welcome from './component/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Hello />
+      <Welcome />
     </div>
   );
 }
---
+++ src/component/Welcome.js
@@ -0,0 +1,19 @@
+// const Welcome = function () {
+//     return(
+//         <h2>Welcome</h2>
+//     );
+// };
+
+
+
+// const Welcome = ()=> {
+//     return(
+//         <h2>Welcome</h2>
+//     );
+// };
+
+export default function Welcome() {
+    return(
+        <h2>Welcome</h2>
+    );
+};
