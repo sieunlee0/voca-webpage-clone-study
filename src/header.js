e777109a4783636a64266b186a55f3def33e724e
sieunlee <dltldmsm@gmail.com>
2021-09-20 11:22:08 +0900

    210920

---
+++ src/header.js
@@ -0,0 +1,16 @@
+// const Header = function() {
+//     return (<div className="header"></div>);
+// }
+
+
+// const Header = () => {
+//     return (
+//         <div className="header"></div>
+//     );
+// }
+
+export default function Header() {
+    return(
+        <div className=" header"></div>
+    )
+};
