710fd51ade09bfeaabc3f16fa09a36ecba866c58
sieunlee <dltldmsm@gmail.com>
2021-09-23 21:28:17 +0900

    210923

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {ReactDOM} from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {App} from 'react';
+import {reportWebVitals} from 'react';
 
-ReactDOM.render(
+ReactDOM.render (
   <React.StrictMode>
     <App />
   </React.StrictMode>,
