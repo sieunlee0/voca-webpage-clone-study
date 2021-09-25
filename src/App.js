10165a23a4ffc871266c6bba5538957775fd78f5
sieunlee <dltldmsm@gmail.com>
2021-09-25 18:22:03 +0900

    210925

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './component/Header';
 import Daylist from './component/Daylist';
 import Day from './component/Day';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import EmptyPage from './component/EmptyPage';
 
 // 모듈을 가져올 때는 from의 내용을 파일의 주소로 쓰지 않아도 되나보다.
 // package.json 파일에 "dependencies" 중 "react-router-dom" 이 있는지 확인해 본다. 설치가 되어있는지 확인할 수 있는 방법이 있었다!
@@ -30,6 +31,9 @@ function App() {
           </Route>
           <Route path="/day/:day">
           <Day />
+          </Route>
+          <Route>
+            <EmptyPage />
           </Route>
         </Switch>
       </div>
