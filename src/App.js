76f702256db7b0156fd06643ec6b6f6f1b9b6541
sieunlee <dltldmsm@gmail.com>
2021-09-25 17:23:02 +0900

    210925

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 /*모듈을 가져올 때는 from의 내용을 파일의 주소로 쓰지 않아도 되나보다.
 package.json 파일에 "dependencies" 중 "react-router-dom" 이 있는지 확인해 본다. 설치가 되어있는지 확인할 수 있는 방법이 있었다!
 
-<qeustion!!!!!!>
-(1)
+Q
 {} 변수 등을 써서 내용을 가져오는 것으로 알고 있다. 
 그래서 모듈의 내용 중에서 몇가지만 가져와서 쓰기 위해 쓸 내용을 {}에 넣어 가져오는 것이 아닌가 한다. 맞나?
-(2)
-BrouwserRouter, Router, Switch가 무엇일까?
-일단 기본적으로 인터넷에 검색을 해보니 routing이란 다른 주소에 다른 화면을 보여 주는 것이라고 한다.*/
+
+A
+맞음. react-router-dom에는 brouserRouter, route, switch에 대한 기능이 담겨 있고 각 기능에는 기능에 따른 정보들이 담겨 있을 것임.
+각 기능들이 컴포넌트로써 가져와지는 것으로 볼 수 있겠음 */
 
 
 function App() {
@@ -37,6 +37,11 @@ function App() {
 
 export default App;
 /*route에 대한 공부 내용
+Q
+BrouwserRouter, Router, Switch가 무엇일까?
+일단 기본적으로 인터넷에 검색을 해보니 routing이란 다른 주소에 다른 화면을 보여 주는 것이라고 한다.
+
+A
 routing(라우팅)이란 주소에 따라 각기 다른 페이지와 그에 따른 내요을 보여주게 하는 것.
 
 Switch 내부는 url을 따라 각각 다른 페이지를 보여주게 되고 외부는 모든 페이지에 공통으로 보여지게 된다.
