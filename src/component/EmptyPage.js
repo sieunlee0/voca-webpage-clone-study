c62c9cefce369f2214b575c45abd8cdf948a96bc
sieunlee <dltldmsm@gmail.com>
2021-09-26 19:21:56 +0900

    210926

--- src/component/EmptyPage.js
+++ src/component/EmptyPage.js
@@ -7,5 +7,10 @@ export default function EmptyPage() {
             <h2>Wrong acception!</h2>
             <Link to="/">Back to the first page</Link>
         </body>
-    )
+    );
 }
+
+/*
+<body>태그가 사실은 강의에서는 <> 이렇게 그저 비어 있는 태그 였는데,
+내꺼에서는 오류로 떠서 일단 body태그로 대체 했다.
+*/
