97de3d2d627f5ae81b2bdfb1ca7a9058f08ef691
sieunlee <dltldmsm@gmail.com>
2021-09-27 22:50:23 +0900

    210927

--- src/component/Word.js
+++ src/component/Word.js
@@ -21,7 +21,12 @@ export default function Word({ word }) {
             <td>{isShow && word.kor}</td>
             <td>
                 <button onClick={ toggleShow }>뜻 {isShow ? "숨기기" : "보기" }</button>
-                {/* 위의 {isShow ? "숨기기" : "보기" }에서 ?는 무슨 의미지?  */}
+                {/* Q: 위의 {isShow ? "숨기기" : "보기" }에서 ?는 무슨 의미지?
+                A: 내가 이해한 바로는 isShow가 어떨 때는 false고 어떨 때는 true가 될 수 있다는 의미이고,
+                그 값이 바뀔 때마다 "숨기기"와 "보기"가 왔다갔다 하게 만들어 둔 것 같다.
+                클릭 이벤트가 발생할 때, true가 돠고 먼저 쓰인 "숨기기" 보인다.
+                다시 클릭하면 false가 되고 "보기"로 바뀐다.
+                 */}
                 <button className="btn_del">삭제</button>
             </td>
         </tr>
