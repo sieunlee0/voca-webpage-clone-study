666040f9b06a6b15ac11b1765f646b4117c53af4
sieunlee <dltldmsm@gmail.com>
2021-09-27 22:35:36 +0900

    210927

--- src/component/Word.js
+++ src/component/Word.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function Word({ word }) {
     const [isShow, setIsShow] = useState(false);
+    // 초기값을 false와 같은 boolea형식으로 주는 것과 다른 형식과의 차이는 무엇일까?
     return(
         <tr>
             <td>
