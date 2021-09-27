2d2106bcbdea271580f26393ff39cc6205ee8547
sieunlee <dltldmsm@gmail.com>
2021-09-27 22:41:55 +0900

    210927

--- src/component/Word.js
+++ src/component/Word.js
@@ -7,6 +7,11 @@ export default function Word({ word }) {
     A: 위의 state를 만든 이유는 버튼을 눌렀을 때 단어의 뜻이 보이게 하려는 것이다.
     따라서, 처음에는 단어의 뜻이 보이지 말아야 하므로 안 보이게 false로 해놓은 것이다.
     */
+    
+    function toggleShow() {
+        setIsShow(!isShow)
+    }
+
     return(
         <tr>
             <td>
@@ -15,7 +20,7 @@ export default function Word({ word }) {
             <td>{word.eng}</td>
             <td>{isShow && word.kor}</td>
             <td>
-                <button>뜻 보기</button>
+                <button onClick={ toggleShow }>뜻 보기</button>
                 <button className="btn_del">단어 삭제</button>
             </td>
         </tr>
