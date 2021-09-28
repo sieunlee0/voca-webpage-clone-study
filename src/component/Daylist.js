51a0177dcc88c0a30277b661dfa4e356ee1bc0ec
sieunlee <dltldmsm@gmail.com>
2021-09-28 20:45:02 +0900

    210928

--- src/component/Daylist.js
+++ src/component/Daylist.js
@@ -10,12 +10,12 @@ export default function Daylist() {
 
     useEffect(() => {
         fetch("http://localhost:3001/days")
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setDays(data);
-        });
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setDays(data);
+            });
     }, []);
     // 만약 fetch()를 useEffect 없이 사용한다면 어떻게 될까? 
 
