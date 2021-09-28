1129c3311adbaab7d05889228a36166f928897cb
sieunlee <dltldmsm@gmail.com>
2021-09-28 21:02:47 +0900

    210928

--- src/component/Daylist.js
+++ src/component/Daylist.js
@@ -16,8 +16,8 @@ export default function Daylist() {
         .then(data => {
             setDays(data);
         });
-    },);
-    // 만약 fetch()를 useEffect 없이 사용한다면 어떻게 될까? 
+    }, []);
+    //] 만약 fetch()를 useEffect 없이 사용한다면 어떻게 될까? 
 
     return(
         <ul className="list_day">
