15500e44de95d141f48721be8e529118ac880fb8
sieunlee <dltldmsm@gmail.com>
2021-09-29 18:53:53 +0900

    210929

--- src/component/Day.js
+++ src/component/Day.js
@@ -15,7 +15,6 @@ function Day() {
 */
 
 import React from 'react';
-import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 /* Q: useParams는 무엇일까?
 A: router의 path에 적은 주소를 가져오는 hook인 듯 하다. */
@@ -35,17 +34,7 @@ export default function Day() {
     // const wordList = dummy.words.filter(word => word.day === Number(day));
     /* useParams가 주소를 가져오는데 주소는 string으로 되어 있다. 
     word.day는 number이기 때문에 useParams로 가져온 주소에 있는 day를 number로 바꿔 주어야 한다.*/
-    const [words, setWords] = useState([]);
-
-    useEffect(() => {
-        fetch(`http://localhost:3001/words?day=${day}`)
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setWords(data);
-        });
-    }, [day]);
+    const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
     return(
         <body>
