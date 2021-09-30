ecf1989f8cf60fab76f52346ebacb8b531842050
sieunlee <dltldmsm@gmail.com>
2021-09-30 18:44:41 +0900

    210930

--- src/component/CreateWord.js
+++ src/component/CreateWord.js
@@ -7,8 +7,16 @@ export default function CreateWord() {
                 <label>eng</label>
                 <iput type="text" placeholder="computer" />
             </div>
-            <div className="input_area"></div>
-            <div className="input_area"></div>
+            <div className="input_area">
+                <label>kor</label>
+                <iput type="text" placeholder="컴퓨터" />
+            </div>
+            <div className="input_area">
+                <select>
+                    <option>1</option>
+                    <option>2</option>
+                </select>
+            </div>
             <button>save</button>
         </from>
     )
