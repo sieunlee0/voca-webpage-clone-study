6c8b9f5035d53275c66a7ad3b473c9caa45085b0
sieunlee <dltldmsm@gmail.com>
2021-10-06 13:48:52 +0900

    211006

--- src/component/ChangePage.js
+++ src/component/ChangePage.js
@@ -7,7 +7,7 @@ export default function ChangePage() {
 
 
     return(
-        <div>
+        <div className="chagePage">
             <button className="leftPage" onClick= { nextPage }> ◀ </button>
             <button className="rightPage" onClick= { nextPage }> ▶ </button>
         </div>
--- src/component/Day.js
+++ src/component/Day.js
@@ -43,8 +43,8 @@ export default function Day() {
 
     return(
         <body>
-            <h2 className="dayNumber">Day {day} </h2>
-            <table className="wordTable">
+            <h2>Day {day} </h2>
+            <table>
                 <tbody>
                     {words.map(word => (
                         <Word word={ word } key={ word.id }/>
