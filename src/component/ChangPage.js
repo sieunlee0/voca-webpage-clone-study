6a234239382f4a973eac1177ac35b03f3bbdc777
sieunlee <dltldmsm@gmail.com>
2021-10-07 20:25:56 +0900

    211007

--- src/component/ChangPage.js
+++ src/component/ChangPage.js
@@ -10,20 +10,19 @@ import { useState } from "react";
 export default function ChangePage({ days: d }) {
     // const { day } = useParams();
     const [ days, setDays ] = useState(d);
-    const history = useHistory();
-    
+    // const history = useHistory();
     // const day = daysData.day;
 
-    function dayPlus() {
-        const dayPlus = Number(day) + 1
-        // history.push(`/day/${dayPlus}`);
-        console.log(dayPlus);
-    }
+    // function dayPlus() {
+    //     const dayPlus = Number(day) + 1
+    //     // history.push(`/day/${dayPlus}`);
+    //     console.log(dayPlus);
+    // }
     
     return(
         <div className="chagePage">
             <button id="leftPage"> ◀ </button>
-            <button id="rightPage" onClick={dayPlus}> ▶ </button>
+            <button id="rightPage"> ▶ </button>
         </div>
     );
 }
