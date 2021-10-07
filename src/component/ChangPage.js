f33120b9cb9fd286a66dfbda60c6bf1c80f6a6e6
sieunlee <dltldmsm@gmail.com>
2021-10-07 22:00:31 +0900

    211007

--- src/component/ChangPage.js
+++ src/component/ChangPage.js
@@ -1,22 +1,33 @@
 import React from "react";
 import { useParams } from 'react-router-dom';
 import { useHistory } from "react-router";
+import useFetch from "../hooks/useFetch";
 
 
 export default function ChangePage() {
     const { day } = useParams();
     const history = useHistory();
+    const days = useFetch("http://localhost:3001/days");
 
     function nextPage(){
         const plusDay = Number(day)+1;
         const nextDay = JSON.stringify(plusDay);
         history.push(`/day/${nextDay}`);
+
+        if(day > days.length-1 ){
+            history.push(`/error`);
+        }
     }
+
 
     function previousPage(){
         const minusDay = Number(day)-1;
         const preDay = JSON.stringify(minusDay);
         history.push(`/day/${preDay}`);
+
+        if(minusDay < 1 ){
+            history.push(`/error`);
+        }
     }
     
 
