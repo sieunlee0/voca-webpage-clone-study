7467bc5892be87141a9de0a06ccec5a5d98d1b30
sieunlee <dltldmsm@gmail.com>
2021-10-07 21:37:06 +0900

    211007

--- src/component/ChangPage.js
+++ src/component/ChangPage.js
@@ -1,19 +1,26 @@
 import React from "react";
 // import dummy from "../db/data.json"
 // import {Link} from "react-router-dom";
-// import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useHistory } from "react-router";
-import { useState } from "react";
+// import { useState } from "react";
 
 
-export default function ChangePage({ days: d }) {
-    const [ days, setDays ] = useState(d);
+export default function ChangePage() {
+    const { day } = useParams();
+    // const [ curDay, setCurDay ] = useState(day);
     const history = useHistory();
 
     function change(){
-        const changeDay = setDays(days+1);
-        const newDay = JSON.stringify(changeDay);
+        // setCurDay(()=>
+        //     Number(curDay)+1
+        // );
+        // const changeDay = setDay(day+1);
+        // const newDay = JSON.stringify(curDay);
+        const plusDay = Number(day)+1;
+        const newDay = JSON.stringify(plusDay);
         history.push(`/day/${newDay}`);
+        console.log(newDay);
     }
     
 
