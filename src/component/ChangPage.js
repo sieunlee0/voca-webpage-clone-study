60bb4af76fe5688ba40bfdf4cafc1584d156b6b6
sieunlee <dltldmsm@gmail.com>
2021-10-07 21:13:23 +0900

    211007

--- src/component/ChangPage.js
+++ src/component/ChangPage.js
@@ -2,15 +2,17 @@ import React from "react";
 // import dummy from "../db/data.json"
 // import {Link} from "react-router-dom";
 // import { useParams } from 'react-router-dom';
-// import { useHistory } from "react-router";
+import { useHistory } from "react-router";
 import { useState } from "react";
 
 
 export default function ChangePage({ days: d }) {
     const [ days, setDays ] = useState(d);
+    const history = useHistory();
 
     function change(){
-        
+        const newDay = setDays(days+1);
+        history.push(`/day/${newDay}`);
     }
     
 
@@ -18,7 +20,7 @@ export default function ChangePage({ days: d }) {
     return(
         <div className="chagePage">
             <button id="leftPage"> ◀ </button>
-            <button id="rightPage"> ▶ </button>
+            <button id="rightPage" onClick={change}> ▶ </button>
         </div>
     );
 }
