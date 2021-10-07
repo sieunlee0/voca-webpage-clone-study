9a2a73c31e82ed0ed75b1d0a146a67e967fe2507
sieunlee <dltldmsm@gmail.com>
2021-10-07 20:50:54 +0900

    211007

--- src/component/ChangPage.js
+++ src/component/ChangPage.js
@@ -9,17 +9,12 @@ import { useState } from "react";
 export default function ChangePage({ days: d }) {
     const [ days, setDays ] = useState(d);
     
-    function setDays() {
-        fetch(`http://localhost:3001/days/${days.day}`,{
-            method: 'PUT'
-        });
 
-    }
     
     return(
         <div className="chagePage">
             <button id="leftPage"> ◀ </button>
-            <button id="rightPage" onClick={setDays}> ▶ </button>
+            <button id="rightPage" onClick={}> ▶ </button>
         </div>
     );
 }
