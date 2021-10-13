35d77c74aa4fd68ea6f6857e746c31b43cef7978
sieunlee <dltldmsm@gmail.com>
2021-10-13 16:52:09 +0900

    211013

--- src/component/Word.js
+++ src/component/Word.js
@@ -9,8 +9,7 @@ export default function Word({ word : w }) {
     const [isShow, setIsShow] = useState(false);
     /*Q: 초기값을 false로 주는 이유가 무엇일까?
     A: 위의 state를 만든 이유는 버튼을 눌렀을 때 단어의 뜻이 보이게 하려는 것이다.
-    따라서, 처음에는 단어의 뜻이 보이지 말아야 하므로 안 보이게 false로 해놓은 것이다.
-    */
+    따라서, 처음에는 단어의 뜻이 보이지 말아야 하므로 안 보이게 false로 해놓은 것이다.*/
     const [isDone, setisDone] = useState(word.isDone);
 
 
